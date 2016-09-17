24e6da798adb88a51bfd7723dce22879d495cabe
mike <mike@nau.edu>
2016-09-16 21:22:28 -0700

    made changes to test.sh file

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@
 
 make clean && make &&
 echo testing 3 to 3 &&
-./ppmrw 3 data/test_ascii.ppm out.ppm &&
+./ppmrw 3 data/test_ascii.ppm out1.ppm &&
 echo testing 3 to 6 &&
-./ppmrw 6 data/test_ascii.ppm out.ppm &&
+./ppmrw 6 data/test_ascii.ppm out2.ppm &&
 echo testing 6 to 3 &&
-./ppmrw 3 data/test_raw.ppm out.ppm &&
+./ppmrw 3 data/test_raw.ppm out3.ppm &&
 echo testing 6 to 6 &&
-./ppmrw 6 data/test_raw.ppm out.ppm &&
+./ppmrw 6 data/test_raw.ppm out4.ppm &&
 echo done
